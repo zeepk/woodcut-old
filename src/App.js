9cea6c222661007e613a7becafce52b10c519488
Matt Hughes <zeepkdb@gmail.com>
2020-09-16 21:27:52 -0700

    library

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import CustomNavbar from './components/CustomNavbar';
 import Footer from './components/Footer';
 import CustomRouter from './components/CustomRouter';
 import { BrowserRouter as Router } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
 import 'primereact/resources/themes/luna-pink/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
+function initializeReactGA() {
+	ReactGA.initialize('UA-178282727-1');
+	ReactGA.pageview('/homepage');
+}
+initializeReactGA();
 function App() {
 	return (
 		<div className="App">
