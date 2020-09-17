5f3fe1209dba77a9d777f32ed4c7f7b13fe416bc
Matt Hughes <zeepkdb@gmail.com>
2020-09-16 21:29:16 -0700

    log

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import Footer from './components/Footer';
 import CustomRouter from './components/CustomRouter';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ReactGA from 'react-ga';
-
 import 'primereact/resources/themes/luna-pink/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeflex/primeflex.css';
+console.info(
+	"%c👋 Hi there! You must know your way around if you're seeing this, hats off to you. ",
+	'background: #292b4a; color: white; font-size: medium'
+);
 function initializeReactGA() {
 	ReactGA.initialize('UA-178282727-1');
 	ReactGA.pageview('/homepage');
